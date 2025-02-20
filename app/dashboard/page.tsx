8e3cc1e15168f8d3741ff1d5df6961fea331b5c8
Rohit <rohitdhanotia2003@gmail.com>
2025-02-20 11:24:52 +0530

    Added Template Search Funtionality

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,12 @@
 "use client";
+import React, { useState } from "react";
+import SearchSection from "./_components/SearchSection";
 
 function Dashboard() {
+  const [searchInput, setSearchInput] = useState<string>("");
   return (
     <div className="h-full bg-slate-100">
+      <SearchSection onSearchInput={(value: string) => setSearchInput(value)} />
     </div>
   );
 }
