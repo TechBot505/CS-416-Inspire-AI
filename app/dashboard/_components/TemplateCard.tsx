048b7e12c25d0099694770c5a682755462b9ab18
Rohit <rohitdhanotia2003@gmail.com>
2025-02-20 11:43:52 +0530

    Integrated Gemini Flash for content generation and create model schema, Integrated Text/Markdown Editor service

--- app/dashboard/_components/TemplateCard.tsx
+++ app/dashboard/_components/TemplateCard.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { TEMPLATE } from "./TemplateListSection";
 import Image from "next/image";
+import Link from "next/link";
 
 function TemplateCard(item: TEMPLATE) {
 
   return (
+    <Link href={`/dashboard/content/${item.slug}`} key={item.slug}>
       <div className="sm:p-5 p-4 shadow-md rounded-md border dark:bg-gray-700 bg-white flex flex-col gap-2 cursor-pointer hover:scale-105 transition-all">
         <div className="flex gap-2 items-start justify-between">
           <Image src={item.icon} alt={item.name} width={50} height={50} />
@@ -12,6 +14,7 @@ function TemplateCard(item: TEMPLATE) {
         <h2 className="font-medium text-lg">{item.name}</h2>
         <p className="text-gray-500 dark:text-gray-300 line-clamp-3">{item.description}</p>
       </div>
+      </Link>
   );
 }
 
---
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+import React, { useState } from "react";
+import FormSection from "../_components/FormSection";
+import OutputSection from "../_components/OutputSection";
+import { TEMPLATE } from "../../_components/TemplateListSection";
+import Templates from "@/app/(data)/Templates";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import { chatSession } from "@/utils/AiModels";
+
+interface PROPS {
+  params: {
+    "template-slug": string;
+  };
+}
+
+function CreateNewContent(props: PROPS) {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [aiResponse, setAiResponse] = useState<string>("");
+
+  const selectedTemplate: TEMPLATE | undefined = Templates?.find(
+    (template) => template.slug === props.params["template-slug"]
+  );
+
+  /**
+   * Used to generate content from AI
+   * @param formData 
+   * @returns 
+   */
+  const generateAIContent = async (formData: any) => {
+    setLoading(true);
+    const prompt = selectedTemplate?.aiPrompt;
+    const finalPrompt = JSON.stringify(formData) + ", " + prompt;
+    const result = await chatSession.sendMessage(finalPrompt);
+    setAiResponse(result?.response.text());
+    setLoading(false);
+  };
+
+  return (
+    <div className="flex flex-col p-5 h-full dark:bg-darkPrimary bg-slate-100">
+      <div className="w-5">
+        <Link href="/dashboard">
+          <Button className="flex gap-2 dark:text-white">
+            <ArrowLeft />
+            Back
+          </Button>
+        </Link>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-10 py-5">
+        <FormSection
+          selectedTemplate={selectedTemplate}
+          userFormInput={(v: any) => {
+            generateAIContent(v);
+          }}
+          loading={loading}
+        />
+        <div className="md:col-span-2 lg:col-span-2">
+          <OutputSection aiResponse={aiResponse} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateNewContent;
